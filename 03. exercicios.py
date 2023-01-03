b6d78f1ee2610b037d3a27f2614dfabfa8b5e0cc
LuisLoschi <gustavoloschi362@gmail.com>
2023-01-02 23:13:08 -0300

    modificado

---
+++ 03. exercicios.py
@@ -0,0 +1,17 @@
+'''
+1. Faça um programa que imprima seu nome completo.
+'''
+print("Seu nome completo")
+
+'''
+2. Faça um programa que imprima um numero qualquer.
+'''
+print(340)
+
+'''
+3. Faça um programa que imprima 2 variáveis. Ex : cidade e estado
+'''
+cidade = 'Jundiai'
+estado = 'São Paulo'
+
+print(cidade, estado)
