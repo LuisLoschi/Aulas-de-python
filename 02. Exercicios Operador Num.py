acaab322236cd5f61fbdfb0000a38bb52b6cac13
LuisLoschi <gustavoloschi362@gmail.com>
2023-01-03 21:57:51 -0300

    Aula 2

---
+++ 02. Exercicios Operador Num.py
@@ -0,0 +1,28 @@
+'''
+1. Faça um programa que calcule 7 elevado a potencia 4 ?
+'''
+a = 7
+b = 4
+
+potencia = a ** b
+
+print("resultado é igual a: ", potencia)
+
+'''
+2. Faça um programa que calcule 2022 divido por 15 ?
+'''
+a = 2022
+b = 15
+
+divisao = a / b
+
+print("resultado é igual a: ", divisao)
+
+'''
+3. Faça um programa que calcule a raiz quadrada de 25 ?
+'''
+a= 25
+
+raiz = 25 ** (1/2)
+
+print("Raiz de 25 é igual a: ", raiz)
